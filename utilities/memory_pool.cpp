c371a967ce6f8d90ad07388ddab9bbdce4cefe22
Phil Cooper-King <phil@cooperking.net>
2016-05-25 09:59:52 +0100

    Adding more functionality to memory pool.

--- utilities/memory_pool.cpp
+++ utilities/memory_pool.cpp
@@ -1,4 +1,5 @@
 #include "memory_pool.hpp"
+#include <string.h>
 #include <assert.h>
 
 
@@ -29,8 +30,10 @@ memory_pool_create(const size_t number_of_bytes_to_reserve)
 
 
 memory_chunk
-memory_pool_get_chunk(memory_pool *pool, const size_t request_size)
-{ 
+memory_pool_get_chunk(memory_pool *pool,
+                      const size_t request_size,
+                      const char *name)
+{
   memory_chunk chunk;
   detail::memory_chunk_header *this_header = pool->header;
 
@@ -46,7 +49,8 @@ memory_pool_get_chunk(memory_pool *pool, const size_t request_size)
         
         this_header->size_of_chunk = request_size;
         this_header->available = false;
-        
+        strlcpy(this_header->name, name, sizeof(this_header->name));
+
         uint8_t *next_chunk_start = &reinterpret_cast<uint8_t*>(this_header->start_of_chunk)[this_header->size_of_chunk];
         detail::memory_chunk_header *next_chunk = reinterpret_cast<detail::memory_chunk_header*>(next_chunk_start);
         
@@ -56,8 +60,9 @@ memory_pool_get_chunk(memory_pool *pool, const size_t request_size)
         next_chunk->start_of_chunk = (void*)(next_chunk_start + sizeof(detail::memory_chunk_header));
         
         this_header->next = next_chunk;
-        
-        chunk.chunk_start = this_header->start_of_chunk;
+  
+        chunk.name           = this_header->name;      
+        chunk.chunk_start    = this_header->start_of_chunk;
         chunk.bytes_in_chunk = request_size;
         
         return chunk;
@@ -91,7 +96,8 @@ memory_pool_return_chunk(memory_pool *pool, memory_chunk *chunk)
     if(header->size_of_chunk == chunk->bytes_in_chunk && header->start_of_chunk == chunk->chunk_start)
     {
       header->available = true;
-      
+      strlcpy(header->name, "none", sizeof(header->name)); 
+
       // We need to check if we should merge this chunk
       // with the previous or next chunk.
       detail::memory_chunk_header *prev = header->prev;
@@ -120,6 +126,77 @@ memory_pool_return_chunk(memory_pool *pool, memory_chunk *chunk)
   }
 
   return false;
+}
+
+
+size_t
+memory_pool_get_number_of_chunks(memory_pool *pool)
+{
+  assert(pool);
+
+  size_t i = 0;
+
+  detail::memory_chunk_header *this_header = pool->header;
+
+  if(this_header)
+  {
+    ++i;
+  }
+
+  while(true)
+  {
+    if(this_header->next)
+    {
+      ++i;
+      this_header = this_header->next;
+    }
+    else
+    {
+      break;
+    }
+  }
+
+  return i; 
+}
+
+
+memory_chunk
+memory_pool_get_chunk_by_index(memory_pool *pool, const size_t index)
+{
+  assert(pool);
+
+  size_t curr_i = 0;
+  detail::memory_chunk_header *this_header = pool->header;
+  memory_chunk return_chunk;
+
+  while(true)
+  {
+    if(curr_i == index)
+    {
+      return_chunk.name           = this_header->name;
+      return_chunk.chunk_start    = this_header->start_of_chunk;
+      return_chunk.bytes_in_chunk = this_header->size_of_chunk;
+      return_chunk.in_use         = !this_header->available;     
+    }
+    else if(curr_i > index)
+    {
+      assert(false);
+      // Shouldn't ever happen.
+      break;
+    }
+
+    if(this_header->next)
+    {
+      ++curr_i;
+      this_header = this_header->next; 
+    }
+    else
+    {
+      break;
+    }  
+  }
+
+  return return_chunk;
 }
 
 
